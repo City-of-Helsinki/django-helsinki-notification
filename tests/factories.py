b0c10ceda09c9b02a02ba4b8b534572fb4c8336d
Daniel Prange <daniel.prange@anders.com>
2025-03-13 15:22:53 +0200

    test: add fakers in NotificationFactory
    Refs: KER-454

--- tests/factories.py
+++ tests/factories.py
@@ -9,6 +9,22 @@ from helsinki_notification.models import Notification
 
 @register
 class NotificationFactory(factory.django.DjangoModelFactory):
+    title_fi = factory.Faker("sentence", locale="fi_FI")
+    title_sv = factory.Faker("sentence", locale="sv_SE")
+    title_en = factory.Faker("sentence", locale="en_US")
+
+    external_url_fi = factory.Faker("url", locale="fi_FI")
+    external_url_sv = factory.Faker("url", locale="sv_SE")
+    external_url_en = factory.Faker("url", locale="en_US")
+
+    external_url_title_fi = factory.Faker("sentence", locale="fi_FI")
+    external_url_title_sv = factory.Faker("sentence", locale="sv_SE")
+    external_url_title_en = factory.Faker("sentence", locale="en_US")
+
+    content_fi = factory.Faker("text", locale="fi_FI")
+    content_sv = factory.Faker("text", locale="sv_SE")
+    content_en = factory.Faker("text", locale="en_US")
+
     class Meta:
         model = Notification
 
